ebfd33f22cb7c40323eab356801e85bcbb9a1d16
Daniel Hsu <danielstark1013@gmail.com>
2022-10-26 21:30:57 +0800

    add 2nd ver.

--- chat_analysis.py
+++ chat_analysis.py
@@ -6,6 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0: 
 			print(len(data))
+print('there are total', len(data), 'datas')
 
-
-
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+	
+print('the average length of chat is',sum_len/len(data),)
