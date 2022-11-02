719ae36f70664a55e6ba31712ffaa3ac6cbe12c5
Daniel Hsu <danielstark1013@gmail.com>
2022-11-02 23:01:02 +0800

    add new version

---
+++ chat_analysis_2.py
@@ -0,0 +1,23 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0: 
+			print(len(data))
+print('there are total', len(data), 'datas')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+	
+print('the average length of chat is',sum_len/len(data),)
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('there are total', len(new),'chats are less than 100 chareters')
+print(new[0])
